0621d4e7745d7c74639428bb41215a459ee423de
zhouy526 <zhouy526@newschool.edu>
2023-02-09 14:37:51 -0500

    text78

---
+++ projects/in-praise-of-shadows/styles.js
@@ -0,0 +1,10 @@
+// document.getElementById("yin").addEventListener("mouseover", mouseOver);
+// document.getElementById("demo").addEventListener("mouseout", mouseOut);
+
+// function mouseOver() {//from w w  w .  j a v  a  2  s .  c o  m
+//     document.getElementById("demo").style.color = "red";
+// }
+
+// function mouseOut() {
+//     document.getElementById("demo").style.color = "black";
+// }
