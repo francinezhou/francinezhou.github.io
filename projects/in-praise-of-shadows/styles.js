705a013ba4fb28d09db09ce8432bb76cc9557d99
zhouy526 <zhouy526@newschool.edu>
2023-02-11 14:58:47 -0500

    wrapper

--- projects/in-praise-of-shadows/styles.js
+++ projects/in-praise-of-shadows/styles.js
@@ -1,29 +1,26 @@
-// document.getElementById("yin").addEventListener("mouseover", mouseOver);
-// document.getElementById("demo").addEventListener("mouseout", mouseOut);
-
-// function mouseOver() {//from w w  w .  j a v  a  2  s .  c o  m
-//     document.getElementById("demo").style.color = "red";
-// }
-
-// function mouseOut() {
-//     document.getElementById("demo").style.color = "black";
-// }
-
-
 // Listening to the video element
-let video = document.querySelector("#fog")
+let fog = document.querySelector("#fog")
 let 阝 = document.querySelector ("#_阝")
-let strokes =document.querySelector (".cls-1")
+let 人 = document.querySelector ("#_人")
+let 云 = document.querySelector ("#_云")
+let 医 = document.querySelector ("#_医")
+let 殳 = document.querySelector ("#_殳")
+let 羽 = document.querySelector ("#_羽")
+let 礻 = document.querySelector ("#_礻")
+let 乚 = document.querySelector ("#_乚")
+let 言 = document.querySelector ("#_言")
+let 替 = document.querySelector ("#_替")
+let strokes = document.querySelector (".cls-1")
 
-/* Adding the event listeners on the video to play/pause the video. */
+/* Adding the event listeners on the video to play the video. */
 
 阝.addEventListener("mouseover", function fog (e) {
-    video.style.opacity = "1";
+    document.querySelector("#fog").style.opacity = "1";
  })
 
-/* Applying the mouse out event to pause the video */
+/* Applying the mouse out event to hide the video */
 
 strokes.addEventListener("mouseout", function fog (e) {
-    video.style.opacity = "0";
+    document.querySelector("#fog").style.opacity = "0";
 })
 
