4f1bffe81b6c7ddc6d5167fc7f8178b8b8364ee0
zhouy526 <zhouy526@newschool.edu>
2023-02-21 00:44:41 -0500

    p4

--- projects/in-praise-of-shadows/p4.js
+++ projects/in-praise-of-shadows/p4.js
@@ -2,13 +2,22 @@
   let next = document.querySelector (".next")
   let vasePath = document.querySelector ("#vasePath")
 
-  vasePath.addEventListener("mouseover", function foreground (e) {
+vasePath.addEventListener("mouseover", function foreground (e) {
     document.querySelector(".cls-1").style.opacity = "0";
  })
 
  vasePath.addEventListener("mouseout", function foreground (e) {
   document.querySelector(".cls-1").style.opacity = "1";
 })
+
+vasePath.addEventListener("mouseover", function foreground (e) {
+  document.querySelector(".text4").style.opacity = "1";
+})
+
+vasePath.addEventListener("mouseout", function foreground (e) {
+document.querySelector(".text4").style.opacity = "0";
+})
+
 
 
   
