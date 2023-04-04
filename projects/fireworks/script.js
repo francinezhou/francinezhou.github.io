5d91d089310ad0c9d26634187fe3d9eeead66c44
zhouy526 <zhouy526@newschool.edu>
2023-04-04 07:36:28 -0400

    PERFORMANCE

--- projects/fireworks/script.js
+++ projects/fireworks/script.js
@@ -47,6 +47,79 @@ rocket.addEventListener('click', () => {
 });
 
 
+
+const max_fireworks = 5,
+max_sparks = 10;
+let canvas = document.getElementById('myCanvas');
+let context = canvas.getContext('2d');
+let fws = [];
+
+for (let i = 0; i < max_fireworks; i++) {
+let fw = {
+  sparks: []
+};
+for (let n = 0; n < max_sparks; n++) {
+  let spark = {
+    vx: Math.random() * 5 + .5,
+    vy: Math.random() * 5 + .5,
+    weight: Math.random() * .3 + .03,
+    red: Math.floor(Math.random() * 2),
+    green: Math.floor(Math.random() * 2),
+    blue: Math.floor(Math.random() * 2)
+  };
+  if (Math.random() > .5) spark.vx = -spark.vx;
+  if (Math.random() > .5) spark.vy = -spark.vy;
+  fw.sparks.push(spark);
+}
+fws.push(fw);
+resetFirework(fw);
+}
+window.requestAnimationFrame(explode);
+
+function resetFirework(fw) {
+fw.x = Math.floor(Math.random() * canvas.width);
+fw.y = canvas.height;
+fw.age = 0;
+fw.phase = 'fly';
+}
+
+function explode() {
+context.clearRect(0, 0, canvas.width, canvas.height);
+fws.forEach((fw,index) => {
+  if (fw.phase == 'explode') {
+      fw.sparks.forEach((spark) => {
+      for (let i = 0; i < 10; i++) {
+        let trailAge = fw.age + i;
+        let x = fw.x + spark.vx * trailAge;
+        let y = fw.y + spark.vy * trailAge + spark.weight * trailAge * spark.weight * trailAge;
+        let fade = i * 20 - fw.age * 2;
+        let r = Math.floor(spark.red * fade);
+        let g = Math.floor(spark.green * fade);
+        let b = Math.floor(spark.blue * fade);
+        context.beginPath();
+        context.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',1)';
+        context.rect(x, y, 4, 4);
+        context.fill();
+      }
+    });
+    fw.age++;
+    if (fw.age > 100 && Math.random() < .05) {
+      resetFirework(fw);
+    }
+  } else {
+    fw.y = fw.y - 10;
+    for (let spark = 0; spark < 15; spark++) {
+      context.beginPath();
+      context.fillStyle = 'rgba(' + index * 50 + ',' + spark * 17 + ',0,1)';
+      context.rect(fw.x + Math.random() * spark - spark / 2, fw.y + spark * 4, 4, 4);
+      context.fill();
+    }
+    if (Math.random() < .001 || fw.y < 200) fw.phase = 'explode';
+  }
+});
+window.requestAnimationFrame(explode);
+}
+
 /*  card expand on click */
 let cardSmalls = document.querySelectorAll(".cardSmall");
 function cardSize() {
@@ -57,78 +130,51 @@ let cardLarge = document.querySelector(".cardLarge");
 cardSmall.addEventListener("click", cardSize);
 
 
-// Get all the elements on the page
-const elements = document.getElementsByTagName('*');
+// // Get all the elements on the page
+// const elements = document.getElementsByTagName('*');
 
-// Loop through each element
-for (let i = 0; i < elements.length; i++) {
-  const element = elements[i];
+// // Loop through each element
+// for (let i = 0; i < elements.length; i++) {
+//   const element = elements[i];
 
-  // Only modify elements that contain text
-  if (element.childNodes.length && element.tagName !== 'SCRIPT' && element.tagName !== 'STYLE') {
-    // Loop through each child node of the element
-    for (let j = 0; j < element.childNodes.length; j++) {
-      const node = element.childNodes[j];
+//   // Only modify elements that contain text
+//   if (element.childNodes.length && element.tagName !== 'SCRIPT' && element.tagName !== 'STYLE') {
+//     // Loop through each child node of the element
+//     for (let j = 0; j < element.childNodes.length; j++) {
+//       const node = element.childNodes[j];
 
-      // Replace the letter "Q" with the specified <span> element
-      if (node.nodeType === 3 && node.nodeValue.includes('Q')) {
-        const text = node.nodeValue;
-        const replacedText = text.replace(/Q/g, '<span class="font-replace">Q</span>');
-        const newNode = document.createElement('span');
-        newNode.innerHTML = replacedText;
-        element.replaceChild(newNode, node);
-      }
-    }
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-
-  var element = document.querySelector("#html-content-holder");
-  var getCanvas;
-  
-  html2canvas(element, {
-    onrendered: function(canvas) {
-      document.querySelector("body").appendChild(canvas);
-      getCanvas = canvas;
-    }
-  });
-
-  document.querySelector("#btn-Convert-Html2Image").addEventListener("click", function() {
-    var imageData = getCanvas.toDataURL("image/png");
-    var newData = imageData.replace(/^data:image\/png/, "data:application/octet-stream");
-    this.setAttribute("download", "your_pic_name.png");
-    this.setAttribute("href", newData);
-  });
-
-});
-
-
-// fetch("fireworks.json")
-// .then(function(response){
-// 	return response.json();
-// })
-// .then(function(fireworks){
-// 	let placeholder = document.querySelector("#preview");
-// 	let out = "";
-// 	for(let fireworks of fireworks){
-//     out+= `
-//     <div class="icon">
-// 		<div class="cardLarge" style="background:${rgb()}">
-// 		<div class="image"><img src="${fireworks.imageLink}"></div>
-// 		<div class="category">${fireworks.name || '-'}</div>
-// 		<div class="description">${fireworks.inventory || '-'}</div>
-// 		<div class="year">${fireworks.productCode}</div>
-// 		</div>
-//     </div>`
-// 	}
-
-// 	placeholder.innerHTML = out;
-// });
-//   function rgb() {
-//     const r = Math.floor(Math.random() * 256)
-//     const g = Math.floor(Math.random() * 256)
-//     const b = Math.floor(Math.random() * 256)
-//     return `rgb(${r},${g},${b},.5)`
+//       // Replace the letter "Q" with the specified <span> element
+//       if (node.nodeType === 3 && node.nodeValue.includes('Q')) {
+//         const text = node.nodeValue;
+//         const replacedText = text.replace(/Q/g, '<span class="font-replace">Q</span>');
+//         const newNode = document.createElement('span');
+//         newNode.innerHTML = replacedText;
+//         element.replaceChild(newNode, node);
+//       }
+//     }
 //   }
+// }
+
+// document.addEventListener("DOMContentLoaded", function() {
+
+//   var element = document.querySelector("#html-content-holder");
+//   var getCanvas;
+  
+//   html2canvas(element, {
+//     onrendered: function(canvas) {
+//       document.querySelector("body").appendChild(canvas);
+//       getCanvas = canvas;
+//     }
+//   });
+
+//   document.querySelector("#btn-Convert-Html2Image").addEventListener("click", function() {
+//     var imageData = getCanvas.toDataURL("image/png");
+//     var newData = imageData.replace(/^data:image\/png/, "data:application/octet-stream");
+//     this.setAttribute("download", "your_pic_name.png");
+//     this.setAttribute("href", newData);
+//   });
+
+// });
+
+
 
