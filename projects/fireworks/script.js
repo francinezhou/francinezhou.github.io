56f628c5e5b1d42ba043b87737457d5611480a06
zhouy526 <zhouy526@newschool.edu>
2023-04-05 01:08:26 -0400

    cata

--- projects/fireworks/script.js
+++ projects/fireworks/script.js
@@ -120,6 +120,32 @@ fws.forEach((fw,index) => {
 window.requestAnimationFrame(explode);
 }
 
+
+function createImageOnCanvas(imageId) {
+    //canvas.style.display = "block";
+    //document.getElementById("images").style.overflowY = "hidden";
+    //var img = new Image(300, 300);
+    //img.src = document.getElementById(imageId).src;
+    //context.drawImage(img, (0), (0)); //onload....
+}
+
+function draw(e) {
+    var pos = getMousePos(canvas, e);
+    posx = pos.x;
+    posy = pos.y;
+    context.fillStyle = "#000000";
+    context.fillRect(posx, posy, 4, 4);
+}
+window.addEventListener('mousemove', draw, false);
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+        y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+    };
+}
+
 /*  card expand on click */
 let cardSmalls = document.querySelectorAll(".cardSmall");
 function cardSize() {
