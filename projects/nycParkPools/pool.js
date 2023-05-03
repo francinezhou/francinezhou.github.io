0f93b79c448ede5b54eb337dd5329ec8729b67c6
zhouy526 <zhouy526@newschool.edu>
2023-05-03 19:41:43 -0400

    random

---
+++ projects/nycParkPools/pool.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    leftDiv = createDiv();
+    leftDiv.id('left');
+    rightDiv = createDiv();
+    rightDiv.id('right');
+    for (let i = 0; i < 50; i++) {
+        leftDiv.child(createP('Left content ' + i));
+        rightDiv.child(createP('Right content ' + i));
+    }
+}
+
+function draw() {
+    background(220);
+    noStroke();
+}
+
+function mouseWheel(event) {
+    if (event.x < width/2) {
+        leftDiv.elt.scrollTop += event.deltaY;
+    } else {
+        rightDiv.elt.scrollTop += event.deltaY;
+    }
+}
--- projects/nycParkPools/welcome.js
+++ projects/nycParkPools/welcome.js
@@ -1,4 +1,5 @@
 let jsonData;
+let jsonDataLength;
 let polygons = [];
 
 const boroughs = {
@@ -11,49 +12,97 @@ const boroughs = {
 
 function preload() {
   const url = "https://data.cityofnewyork.us/resource/y5rm-wagw.json";
-  jsonData = loadJSON(url);
+  loadJSON(url, dataLoaded);
+  
+}
+ 
+function dataLoaded(data) {
+  jsonData = Object.values(data);
+  jsonDataLength = jsonData.length;
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  jsonDataLength = Object.keys(jsonData).length;
+  // Find the min/max coordinates of all polygons in the data
+  let minMaxCoords = jsonData.reduce((acc, pool) => {
+    let coords = pool.polygon.coordinates[0]; // get the first set of coordinates in the polygon
+    let longitudes = coords.map(coord => coord[0]); // extract all the longitudes
+    let latitudes = coords.map(coord => coord[1]); // extract all the latitudes
 
-  // Extract coordinates of first polygon from JSON data
-  const polygonCoords = jsonData[0].polygon.coordinates[0];
-  
-  // Convert coordinates to pixels
-  const pixelCoords = polygonCoords.map(coord => {
-    const x = map(coord[0], -74.00398, -74.00342, 0, width);
-    const y = map(coord[1], 40.67293, 40.67321, 0, height);
-    return [x, y];
+    let minLongitude = Math.min(...longitudes);
+    let maxLongitude = Math.max(...longitudes);
+    let minLatitude = Math.min(...latitudes);
+    let maxLatitude = Math.max(...latitudes);
+
+    // update the accumulator with the new min/max values
+    acc.minLongitude = Math.min(acc.minLongitude, minLongitude);
+    acc.maxLongitude = Math.max(acc.maxLongitude, maxLongitude);
+    acc.minLatitude = Math.min(acc.minLatitude, minLatitude);
+    acc.maxLatitude = Math.max(acc.maxLatitude, maxLatitude);
+
+    return acc;
+  }, {
+    // set initial values for the accumulator
+    minLongitude: Infinity,
+    maxLongitude: -Infinity,
+    minLatitude: Infinity,
+    maxLatitude: -Infinity
   });
-  
-  // Add polygon to list of polygons
-  polygons.push(pixelCoords);
-  
-  // Draw polygon
-  beginShape();
-  pixelCoords.forEach(coord => vertex(coord[0], coord[1]));
-  endShape(CLOSE);
-  
-  // Display name of pool
+}
 
-    // textSize(20);
-    // textStyle(BOLD);
-    // textAlign(CENTER, CENTER);
-    const name = jsonData[0].name;
-    // text(name, width/2, height/2);
+let count = Math.floor(Math.random() * 92);
+let isPlaying = 0;
 
-    const borough = boroughs[jsonData[0].borough];
-    const location = jsonData[0].location;
-    const pooltype = jsonData[0].pooltype;
-    // textSize(14);
-    // textStyle(NORMAL);
-    // textAlign(CENTER);
-    // const description = `${borough} / ${location} / ${pooltype}`;
-    // text(description, width/2, height/2 + 25);
+function draw() {
+    //console.log(count)
+    let name = jsonData[count].name;
+    let borough = boroughs[jsonData[count].borough];
+    let location = jsonData[count].location;
+    let pooltype = jsonData[count].pooltype;
+  
+    // Loop through each polygon in the data
+  for (let i = 0; i < jsonDataLength; i++) {
+    let coords = jsonData[i].polygon.coordinates[0]; // get the first set of coordinates in the polygon
+    let longitudes = coords.map(coord => coord[0]); // extract all the longitudes
+    let latitudes = coords.map(coord => coord[1]); // extract all the latitudes
 
+    let minLongitude = min(longitudes);
+    let maxLongitude = max(longitudes);
+    let minLatitude = min(latitudes);
+    let maxLatitude = max(latitudes);
+
+    // Convert coordinates to pixels using the min/max values
+    let pixelCoords = coords.map(coord => {
+      let x = map(coord[0], minLongitude, maxLongitude, 0, width);
+      let y = map(coord[1], minLatitude, maxLatitude, 0, height);
+      return [x, y];
+    });
+
+    // Add polygon to list of polygons
+    polygons.push(pixelCoords);
+
+    // Draw polygon
+    beginShape();
+    pixelCoords.forEach(coord => vertex(coord[0], coord[1]));
+    endShape(CLOSE);
+  }
     document.querySelector('.randomizerResult h2').textContent = name;
     document.querySelector('.randomizerResult p').textContent = `${borough} / ${location} / ${pooltype}`;
+  
+    document.querySelector('.randomizerTitle').addEventListener('click', () => {
+    if (isPlaying === 1) {
 
+      isPlaying = 0;
+      clearInterval(animate);
+    } else {
+
+      isPlaying = 1;
+      animate = setInterval(playAnimation, 100);
+    }
+  })
+}
+
+function playAnimation() {
+  count = Math.floor(Math.random() * jsonDataLength);
 }
