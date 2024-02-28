2a1dfb8926b84d8b5293d21d8ddac6d2ca3a6b04
zhouy526 <zhouy526@newschool.edu>
2024-02-28 00:30:41 -0500

    drag

--- projects/project1/osmanthus/script.js
+++ projects/project1/osmanthus/script.js
@@ -149,3 +149,32 @@ $(document).ready(function () {
  
   
 });
+interact('.draggable')
+  .draggable({
+    inertia: true,
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    autoScroll: true,
+
+    listeners: {
+      move: dragMoveListener,
+
+      end(event) {
+        // Remove the text display code
+      }
+    }
+  });
+
+function dragMoveListener(event) {
+  var target = event.target;
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
+}
