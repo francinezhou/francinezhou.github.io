c70f6f67d3daeea838d71d06d70db298439cedc4
zhouy526 <zhouy526@newschool.edu>
2024-02-29 16:48:13 -0500

    osmanthus

--- projects/project1/osmanthus/script.js
+++ projects/project1/osmanthus/script.js
@@ -13,128 +13,6 @@ if (document.addEventListener) {
   });
 }
 
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 63,
-      "density": {
-        "enable": true,
-        "value_area": 721.5354273894853
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "image",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "data:image/svg+xml;utf8," + encodeURIComponent(`<?xml version="1.0" encoding="UTF-8"?>
-          <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 38.81 38.81">
-            <defs>
-              <style>
-                .cls-1 {
-                  fill: #ffbc00;
-                }
-              </style>
-            </defs>
-            <g id="osmanthus_layer" data-name="osmanthus layer">
-              <path class="cls-1" d="M30,19.4c18.06,15.83,5.23,28.65-10.6,10.6-15.83,18.06-28.65,5.23-10.6-10.6C-9.25,3.58,3.58-9.25,19.4,8.81c15.83-18.06,28.65-5.23,10.6,10.60Z"/>
-            </g>
-          </svg>`),
-        "width": 50,
-        "height": 50
-      }
-    },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 6,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 6,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "bounce", // Set out_mode to "bounce"
-      "bounce": true, // Enable bouncing
-      "attract": {
-        "enable": true,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "bubble"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 170.52652093416066,
-        "size": 9,
-        "duration": 1.0556403676876611,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 113.68434728944044,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-});
 
 
 $(document).ready(function () {
@@ -177,4 +55,115 @@ function dragMoveListener(event) {
   target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
   target.setAttribute('data-x', x);
   target.setAttribute('data-y', y);
+}
+
+// Code ported to Paper.js from http://the389.com/9/1/
+// with permission.
+
+var values = {
+	friction: 0.8,
+	timeStep: 0.01,
+	amount: 15,
+	mass: 2,
+	count: 0
+};
+values.invMass = 1 / values.mass;
+
+var path, springs;
+var size = view.size * [1.2, 1];
+
+var Spring = function(a, b, strength, restLength) {
+	this.a = a;
+	this.b = b;
+	this.restLength = restLength || 80;
+	this.strength = strength ? strength : 0.55;
+	this.mamb = values.invMass * values.invMass;
+};
+
+Spring.prototype.update = function() {
+	var delta = this.b - this.a;
+	var dist = delta.length;
+	var normDistStrength = (dist - this.restLength) /
+			(dist * this.mamb) * this.strength;
+	delta.y *= normDistStrength * values.invMass * 0.2;
+	if (!this.a.fixed)
+		this.a.y += delta.y;
+	if (!this.b.fixed)
+		this.b.y -= delta.y;
+};
+
+
+function createPath(strength) {
+	var path = new Path({
+		fillColor: 'black'
+	});
+	springs = [];
+	for (var i = 0; i <= values.amount; i++) {
+		var segment = path.add(new Point(i / values.amount, 0.5) * size);
+		var point = segment.point;
+		if (i == 0 || i == values.amount)
+			point.y += size.height;
+		point.px = point.x;
+		point.py = point.y;
+		// The first two and last two points are fixed:
+		point.fixed = i < 2 || i > values.amount - 2;
+		if (i > 0) {
+			var spring = new Spring(segment.previous.point, point, strength);
+			springs.push(spring);
+		}
+	}
+	path.position.x -= size.width / 4;
+	return path;
+}
+
+function onResize() {
+	if (path)
+		path.remove();
+	size = view.bounds.size * [2, 1];
+	path = createPath(0.1);
+}
+
+function onMouseMove(event) {
+	var location = path.getNearestLocation(event.point);
+	var segment = location.segment;
+	var point = segment.point;
+
+	if (!point.fixed && location.distance < size.height / 4) {
+		var y = event.point.y;
+		point.y += (y - point.y) / 6;
+		if (segment.previous && !segment.previous.fixed) {
+			var previous = segment.previous.point;
+			previous.y += (y - previous.y) / 24;
+		}
+		if (segment.next && !segment.next.fixed) {
+			var next = segment.next.point;
+			next.y += (y - next.y) / 24;
+		}
+	}
+}
+
+function onFrame(event) {
+	updateWave(path);
+}
+
+function updateWave(path) {
+	var force = 1 - values.friction * values.timeStep * values.timeStep;
+	for (var i = 0, l = path.segments.length; i < l; i++) {
+		var point = path.segments[i].point;
+		var dy = (point.y - point.py) * force;
+		point.py = point.y;
+		point.y = Math.max(point.y + dy, 0);
+	}
+
+	for (var j = 0, l = springs.length; j < l; j++) {
+		springs[j].update();
+	}
+	path.smooth({ type: 'continuous' });
+}
+
+function onKeyDown(event) {
+	if (event.key == 'space') {
+		path.fullySelected = !path.fullySelected;
+		path.fillColor = path.fullySelected ? null : 'black';
+	}
 }
