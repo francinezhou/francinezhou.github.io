9c7a4e719c12ba929fe8a7c2729d1532677ba27c
zhouy526 <zhouy526@newschool.edu>
2024-03-20 00:59:06 -0400

    css

--- projects/project1/osmanthus/js/script.js
+++ projects/project1/osmanthus/js/script.js
@@ -23,11 +23,9 @@ if (document.addEventListener) {
     window.attachEvent("onload", handleDOMContentLoaded);
 }
 
-  
 
 
 $(document).ready(function () {
-
   // Toggle display manually on .jarCap click
   $(".jarCap").click(function() {
     $("#particles-js").toggleClass('show');
@@ -37,67 +35,20 @@ $(document).ready(function () {
 });
 
 
-// target elements with the "draggable" class
-interact('.draggable')
-  .draggable({
-    // enable inertial throwing
-    inertia: true,
-    // keep the element within the area of it's parent
-    modifiers: [
-      interact.modifiers.restrictRect({
-        restriction: 'parent',
-        endOnly: true
-      })
-    ],
-    // enable autoScroll
-    autoScroll: true,
-
-    listeners: {
-      // call this function on every dragmove event
-      move: dragMoveListener,
-
-      // call this function on every dragend event
-      end (event) {
-        var textEl = event.target.querySelector('p')
-
-        textEl && (textEl.textContent =
-          'moved a distance of ' +
-          (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                     Math.pow(event.pageY - event.y0, 2) | 0))
-            .toFixed(2) + 'px')
-      }
-    }
-  })
-
-function dragMoveListener (event) {
-  var target = event.target
-  // keep the dragged position in the data-x/data-y attributes
-  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-  // translate the element
-  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-
-  // update the posiion attributes
-  target.setAttribute('data-x', x)
-  target.setAttribute('data-y', y)
-}
-
-// this function is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener
+// drag drop
 
 interact('.drag-drop')
 .draggable({
-  inertia: true,
-  modifiers: [
-	interact.modifiers.restrictRect({
-	  restriction: 'parent',
-	  endOnly: true
-	})
-  ],
-  autoScroll: true,
-  // dragMoveListener from the dragging demo above
-  listeners: { move: dragMoveListener }
+	inertia: true,
+	modifiers: [
+		interact.modifiers.restrictRect({
+			restriction: 'parent',
+			endOnly: true
+		})
+	],
+	autoScroll: true,
+	// dragMoveListener from the dragging demo above
+	listeners: { move: dragMoveListener }
 })
 
 // enable dropzone
@@ -110,48 +61,45 @@ overlap: 0.75,
 // listen for drop related events:
 
 ondropactivate: function (event) {
-  // add active dropzone feedback
-  event.target.classList.add('drop-active')
+	// add active dropzone feedback
+	event.target.classList.add('drop-active');
 },
 ondragenter: function (event) {
-  var draggableElement = event.relatedTarget
-  var dropzoneElement = event.target
+	var draggableElement = event.relatedTarget;
+	var dropzoneElement = event.target;
 
-  // feedback the possibility of a drop
-  dropzoneElement.classList.add('drop-target')
-  draggableElement.classList.add('can-drop')
-  draggableElement.textContent = 'Dragged in'
+	// feedback the possibility of a drop
+	dropzoneElement.classList.add('drop-target');
+	draggableElement.classList.add('can-drop');
 },
 ondragleave: function (event) {
-  // remove the drop feedback style
-  event.target.classList.remove('drop-target')
-  event.relatedTarget.classList.remove('can-drop')
-  event.relatedTarget.textContent = 'Dragged out'
+	// remove the drop feedback style
+	event.target.classList.remove('drop-target');
+	event.relatedTarget.classList.remove('can-drop');
 },
 ondrop: function (event) {
-  event.relatedTarget.textContent = 'Dropped'
+	// Add bodyColored class to body
+	document.body.classList.add('bodyColored');
 },
 ondropdeactivate: function (event) {
-  // remove active dropzone feedback
-  event.target.classList.remove('drop-active')
-  event.target.classList.remove('drop-target')
+	// remove active dropzone feedback
+	event.target.classList.remove('drop-active');
+	event.target.classList.remove('drop-target');
 }
-})
+});
 
 function dragMoveListener(event) {
-var target = event.target
+var target = event.target;
 // keep the dragged position in the data-x/data-y attributes
-var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
 // translate the element
 target.style.webkitTransform =
-  target.style.transform =
-	'translate(' + x + 'px, ' + y + 'px)'
+	target.style.transform =
+	'translate(' + x + 'px, ' + y + 'px)';
 
 // update the position attributes
-target.setAttribute('data-x', x)
-target.setAttribute('data-y', y)
+target.setAttribute('data-x', x);
+target.setAttribute('data-y', y);
 }
-
-
